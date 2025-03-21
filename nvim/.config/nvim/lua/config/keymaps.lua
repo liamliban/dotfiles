17f1bc79a2b401156f4513facd3f7f464434cbff
Liam <liamliban1229@gmail.com>
2025-03-21 09:35:31 +0800

    Add: Select all keymap

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,14 @@
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
 
 -- Select all
-vim.api.nvim_set_keymap("n", "<M-a>", "ggVG", { desc = "Select all" })
+-- vim.keymap.set("n", "<M-a>", "ggVG", { desc = "Select All" })
+vim.keymap.set("n", "<M-a>", function()
+  vim.g.snacks_animate = false
+  vim.schedule(function()
+    vim.cmd("normal! ggVG")
+    vim.g.snacks_animate = true
+  end)
+end, { desc = "Select All" })
 
 -- Notify on save
 vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", function()
